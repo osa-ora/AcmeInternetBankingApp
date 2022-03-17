09133b823be47cc26f1c3915dd37238002317146
Osama Oransa <osa_ora@yahoo.com>
2022-03-17 12:11:03 +0200

    Update init-deployment.sh

--- configs/init-deployment.sh
+++ configs/init-deployment.sh
@@ -6,10 +6,9 @@ read
 oc new-project dev
 oc new-app --name=my-gui-app jboss-webserver31-tomcat8-openshift:1.4~https://github.com/osa-ora/AcmeInternetBankingApp
 oc apply -f https://raw.githubusercontent.com/osa-ora/AcmeInternetBankingApp/master/configs/configmap.yaml
-oc get ksvc 
-echo "Please edit the configmap 'env-settings' with correct serverless local endpoints"  
+oc expose service my-gui-app
+echo "Please make sure the configmap 'env-settings' has the correct serverless local endpoints"  
 echo "Press [Enter] key to resume..." 
 read
 oc set env deployment/my-gui-app --from configmap/env-settings
-oc expose service my-gui-app
 echo "Internet Banking GUI deployed successfully as a serverless" 
